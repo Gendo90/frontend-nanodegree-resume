affa6b1280d7e0d3023378adaa4f180282f3d269
Patrick Gendotti <patrick.gendotti@gmail.com>
2019-08-30 14:48:06 -0700

    feat: Add colors to rangoli and add rangoli to website as a link on the Artwork page

--- js/get_rangoli.js
+++ js/get_rangoli.js
@@ -7,6 +7,9 @@ let submit_button = document.getElementById("submit");
 let rangoli_parent = document.getElementById("rangoli_space")
 let output_lines;
 let para;
+let curr_span;
+let CSS_COLOR_NAMES = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","DarkOrange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","RebeccaPurple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"];
+let seen_chars = new Map()
 
 let first_state = ['--------e--------',
                     '------e-d-e------',
@@ -32,6 +35,11 @@ let params = {
     variables: 10       //# (optional)
 }
 
+function getRandomColor() {
+    ind = Math.floor(Math.random()*CSS_COLOR_NAMES.length)
+    return CSS_COLOR_NAMES[ind]
+}
+
 submit_button.addEventListener("click", function() {
     size = document.getElementById("input_size").value;
     //$.ajax call here!
@@ -42,14 +50,28 @@ submit_button.addEventListener("click", function() {
         }
     ).done(function() {
         console.log(rangoli_result)
+        seen_chars.clear()
         $("#rangoli_space").empty();
         output_lines = rangoli_result.responseJSON.Result.body
         console.log(output_lines)
+        let this_line_arr;
+        let this_span_collection;
         for (let line of output_lines.results) {
             para = document.createElement("p");
             para.style.fontFamily = "Lucida Console"
             para.classList.add("rangoli");
-            para.textContent = line;
+            this_line_arr = line.split('')
+            for (let char of this_line_arr) {
+                if(!seen_chars.has(char)) {
+                    seen_chars.set(char, getRandomColor());
+                }
+                curr_span = document.createElement("span")
+                curr_span.style.color = seen_chars.get(char)
+                curr_span.style.padding = 0
+                curr_span.textContent = char
+                para.appendChild(curr_span);
+            }
+            // para.textContent = line;
             rangoli_parent.appendChild(para);
         }
 })
