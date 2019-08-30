fdc07356aa236a8fd4e079ab0fe91f474b45ea7d
Patrick Gendotti <patrick.gendotti@gmail.com>
2019-08-29 21:09:29 -0700

    feat: Add capability to stop animation and touch scroll the nav bar on the homepage for an iPad device

--- js/logo_anim.js
+++ js/logo_anim.js
@@ -8,3 +8,18 @@ logo_img.addEventListener("click", function() {
         logo.style.animationPlayState = "paused"
     }
 })
+
+
+const nav_bar = document.getElementsByTagName("nav");
+const page_links = document.querySelectorAll("div.lower_pic_container");
+const lower_flex = document.getElementsByClassName("lower_pic_flex")[0];
+
+nav_bar[0].addEventListener("touchstart", function() {
+    for (let link of page_links) {
+        console.log(link)
+        if(!link.classList.contains("stop_animation")) {
+            link.classList.add("stop_animation");
+        }
+    }
+    lower_flex.style.overflow = "auto";
+})
